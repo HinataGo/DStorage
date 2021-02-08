409328c81e01e51dbae9b1eda51e31ce64ceab73
HinataGo <365264652@qq.com>
2021-02-08 15:11:46 +0800

    1.0 信息加密验证

---
+++ encrypt/md5.go
@@ -0,0 +1,41 @@
+package encrypt
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func MD5(data []byte) string {
+	_md5 := md5.New()
+	_md5.Write(data)
+	return hex.EncodeToString(_md5.Sum([]byte("")))
+}
+
+func FileMD5(file *os.File) string {
+	_md5 := md5.New()
+	io.Copy(_md5, file)
+	return hex.EncodeToString(_md5.Sum(nil))
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func GetFileSize(filename string) int64 {
+	var result int64
+	filepath.Walk(filename, func(path string, f os.FileInfo, err error) error {
+		result = f.Size()
+		return nil
+	})
+	return result
+}
